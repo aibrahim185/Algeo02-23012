60293298e4e9ce64d9a67b50d3a7e1735425533b
root <root@Jarvisss>
2024-11-21 23:47:56 +0700

    fast-api migration

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+from fastapi import FastAPI
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+### Create FastAPI instance with custom docs and openapi url
+app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+
+@app.get("/api/py/helloFastApi")
+def hello_fast_api():
+    return {"message": "Hello from FastAPI"}
--- next.config.js
+++ next.config.js
@@ -3,14 +3,28 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '/api/:path*',
+        source: "/api/py/:path*",
         destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/py/:path*"
+            : "/api/",
       },
-    ]
+      {
+        source: "/docs",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/py/docs"
+            : "/api/py/docs",
+      },
+      {
+        source: "/openapi.json",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/py/openapi.json"
+            : "/api/py/openapi.json",
+      },
+    ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
