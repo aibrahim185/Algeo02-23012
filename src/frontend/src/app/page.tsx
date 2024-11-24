619a3231549b56b0172ded9c711784191eb5a8c9
hima <19623185@std.stei.itb.ac.id>
2024-11-25 04:08:58 +0700

    feat: add CORS middleware to FastAPI and implement API data fetching in frontend

--- src/frontend/src/app/page.tsx
+++ src/frontend/src/app/page.tsx
@@ -1,9 +1,29 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
+        method: "GET",
+      });
+      console.log(res);
+      const data = await res.json();
+      setMessage(data.Hello);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="grid place-content-center min-h-screen w-full">
       <Image src={"/ambalabu-text.png"} alt={"bg"} width={1000} height={1000} />
+      <h1>{message ? message : "Loading..."}</h1>
+      {process.env.NEXT_PUBLIC_API_URL}
     </div>
   );
 }
