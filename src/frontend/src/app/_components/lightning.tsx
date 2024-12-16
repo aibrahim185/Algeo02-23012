4dda4aed2a07aeb2340a78713b58e8b3cde54d3d
hima <19623185@std.stei.itb.ac.id>
2024-12-16 07:32:17 +0700

    feat: enhance lightning component with bloody and jumpscare effects

--- src/frontend/src/app/_components/lightning.tsx
+++ src/frontend/src/app/_components/lightning.tsx
@@ -1,31 +1,58 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Lightning() {
   const [isLightning, setIsLightning] = useState(false);
+  const [isBloody, setIsBloody] = useState(false);
+  const [isJumpscare, setIsJumpscare] = useState(false);
 
   useEffect(() => {
     const triggerLightning = () => {
+      setIsBloody(false);
+      setIsJumpscare(false);
       setIsLightning(true);
       setTimeout(() => setIsLightning(false), 150);
       setTimeout(() => {
         setIsLightning(true);
-        setTimeout(() => setIsLightning(false), 100);
+        setTimeout(() => {
+          setIsLightning(false);
+          if (Math.random() < 0.2) {
+            if (Math.random() < 0.5) {
+              setIsBloody(true);
+            } else {
+              setIsJumpscare(true);
+            }
+          }
+        }, 100);
       }, Math.random() * 200 + 100);
     };
 
     const lightningInterval = setInterval(() => {
       triggerLightning();
-    }, Math.random() * 8000 + 3000);
+    }, Math.random() * 1 + 3000);
 
     return () => clearInterval(lightningInterval);
   }, []);
   return (
     <div
-      className={`h-full w-full  inset-0 absolute -z-50 ${
+      className={`size-full inset-0 absolute -z-50 ${
         isLightning ? "bg-white" : ""
       }`}
-    ></div>
+    >
+      {isBloody && (
+        <div className="size-full inset-0 absolute bg-[url('/blood.png')] animate-pulse"></div>
+      )}
+      {isJumpscare && (
+        <Image
+          src="/placeholder.png"
+          width={200}
+          height={200}
+          alt="jumpscare"
+          className="w-1/2 h-full absolute inset-0"
+        />
+      )}
+    </div>
   );
 }
